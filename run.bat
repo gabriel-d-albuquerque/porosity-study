5f0b22f45e7e0e15f432ed4ecd29494dec644e45
gabriel-d-albuquerque <gabriel_albuquerque@usp.br>
2021-12-03 11:52:47 -0300

    Created a batch file able to run all Abaqus scripting files.

---
+++ run.bat
@@ -0,0 +1,17 @@
+cd C:\Users\Gabriel Delgado\OneDrive\Universidade\IC\HelloAbaqus\estudo_porosidade\porosity_0\
+call abaqus cae noGUI=script_porosity_0.py
+cd C:\Users\Gabriel Delgado\OneDrive\Universidade\IC\HelloAbaqus\estudo_porosidade\porosity_10\
+call abaqus cae noGUI=script_porosity_10.py
+cd C:\Users\Gabriel Delgado\OneDrive\Universidade\IC\HelloAbaqus\estudo_porosidade\porosity_20\
+call abaqus cae noGUI=script_porosity_20.py
+cd C:\Users\Gabriel Delgado\OneDrive\Universidade\IC\HelloAbaqus\estudo_porosidade\porosity_30\
+call abaqus cae noGUI=script_porosity_30.py
+cd C:\Users\Gabriel Delgado\OneDrive\Universidade\IC\HelloAbaqus\estudo_porosidade\porosity_40\
+call abaqus cae noGUI=script_porosity_40.py
+cd C:\Users\Gabriel Delgado\OneDrive\Universidade\IC\HelloAbaqus\estudo_porosidade\porosity_50\
+call abaqus cae noGUI=script_porosity_50.py
+cd C:\Users\Gabriel Delgado\OneDrive\Universidade\IC\HelloAbaqus\estudo_porosidade\porosity_60\
+call abaqus cae noGUI=script_porosity_60.py
+cd C:\Users\Gabriel Delgado\OneDrive\Universidade\IC\HelloAbaqus\estudo_porosidade\porosity_70\
+call abaqus cae noGUI=script_porosity_70.py
+

d2dd442414129b61f1b21ef46b6fdecee374dba3
gabriel-d-albuquerque <gabriel_albuquerque@usp.br>
2022-02-02 16:02:58 -0300

    Adapted files for the new mesh script.

--- mesh_plot.py
+++ mesh_plot.py
@@ -1,13 +1,13 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
-fd = ['.\mesh_1', '.\mesh_2', '.\mesh_3', '.\mesh_4', '.\mesh_5', '.\mesh_6', '.\mesh_7', '.\mesh_8', '.\mesh_9', '.\mesh_10']
-fl = ['\mesh_1.txt', '\mesh_2.txt', '\mesh_3.txt', '\mesh_4.txt', '\mesh_5.txt', '\mesh_6.txt', '\mesh_7.txt', '\mesh_8.txt', '\mesh_9.txt', '\mesh_10.txt']
+fl = ['\mesh_01.txt', '\mesh_025.txt', '\mesh_05.txt', '\mesh_1.txt', '\mesh_2.txt', '\mesh_3.txt']
 
 S11_values =[]
 S22_values =[]
 x=[]
-for (folder, file) in zip(fd, fl):
-    with open(folder + file) as f:
+for file in fl:
+    with open('.\mesh_study' + file) as f:
         S11=f.readline()
         S22=f.readline()
         S33=f.readline()
@@ -22,20 +22,27 @@ for (folder, file) in zip(fd, fl):
     S22_values.append(S22)
     x.append(s)
 
+S11_values = np.array(S11_values)/S11_values[5]
+S22_values = np.array(S22_values)/S22_values[5]
+
 fig, ax = plt.subplots()
 ax.scatter(x, S11_values, marker=(5, 2), label='S11', color='red')
+ax.scatter(x, S22_values, marker=(5, 2), label='S22', color='blue')
 ax.legend()
-ax.set_title("Effect of mesh refinement on stress")
+ax.set_title("Influence of mesh refinement on stress components")
 ax.set_xlabel("Mesh size [mm]")
-ax.set_ylabel("Stress [MPa]")
+ax.set_ylabel("Relative stress component")
 ax.grid(color="gray", linestyle="--", linewidth=0.7)
+plt.yscale("log")
 plt.show()
 
 fig, ax = plt.subplots()
-ax.scatter(x, S22_values, marker=(5, 2), label='S22', color='red')
+ax.scatter(x, S11_values, marker=(5, 2), label='S11', color='red')
+ax.scatter(x, S22_values, marker=(5, 2), label='S22', color='blue')
 ax.legend()
-ax.set_title("Effect of mesh refinement on stress")
+ax.set_title("Influence of mesh refinement on stress components")
 ax.set_xlabel("Mesh size [mm]")
-ax.set_ylabel("Stress [MPa]")
+ax.set_ylabel("Relative stress component")
 ax.grid(color="gray", linestyle="--", linewidth=0.7)
 plt.show()
+

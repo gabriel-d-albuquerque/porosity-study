adef880a439de6fe0d52fe3a15f62e98628fe769
gabriel-d-albuquerque <gabriel_albuquerque@usp.br>
2022-04-09 18:39:40 -0300

    Adapted the file so that it could have one of its function called by other scripts.

--- gmsh/gera_malha_per_cfc.py
+++ gmsh/gera_malha_per_cfc.py
@@ -116,73 +116,74 @@ def get_face_nodes(coords, ref, dir, eps=1e-4):
 MAIN CODE
  -----------------------------------------'''
 
-# RVE creation with periodicity
-domain = [0, 1, 0, 1, 0, 1]
+if __name__ == "__main__":
+    # RVE creation with periodicity
+    domain = [0, 1, 0, 1, 0, 1]
 
-# CFC - 4 particles - 3 at the faces ( 6 * 1/2) + 1 (8* 1/8) at the corners
-r = 0.207642 #f = 15
-#r = 0.228539 f = 20
-#r = 0.246186  f = 25
-#r = 0.261612 f = 30
-#r = 0.275406 f = 35
-#r = 0.287941  f = 40
-#r = 0.299471 f = 45
-particles =[[0.5,0.5,0.0,r],
-            [0.5,0.5,1.0,r],
-            [0.5,0.0,0.5,r],
-            [0.5,1.0,0.5,r],
-            [0.0,0.5,0.5,r],
-            [1.0,0.5,0.5,r],
-            [1.0,1.0,1.0,r],
-            [1.0,1.0,0.0,r],
-            [1.0,0.0,1.0,r],
-            [0.0,1.0,1.0,r],
-            [1.0,0.0,0.0,r],
-            [0.0,1.0,0.0,r],
-            [0.0,0.0,1.0,r],
-            [0.0,0.0,0.0,r]]
-            
-create_RVE_particles(domain, particles, mesh_size_factor=0.01, element_order=1)
-#gmsh.write("periodic_example.vtk")
-gmsh.write("t_one_incl_15.inp")
+    # CFC - 4 particles - 3 at the faces ( 6 * 1/2) + 1 (8* 1/8) at the corners
+    r = 0.207642 #f = 15
+    #r = 0.228539 f = 20
+    #r = 0.246186  f = 25
+    #r = 0.261612 f = 30
+    #r = 0.275406 f = 35
+    #r = 0.287941  f = 40
+    #r = 0.299471 f = 45
+    particles =[[0.5,0.5,0.0,r],
+                [0.5,0.5,1.0,r],
+                [0.5,0.0,0.5,r],
+                [0.5,1.0,0.5,r],
+                [0.0,0.5,0.5,r],
+                [1.0,0.5,0.5,r],
+                [1.0,1.0,1.0,r],
+                [1.0,1.0,0.0,r],
+                [1.0,0.0,1.0,r],
+                [0.0,1.0,1.0,r],
+                [1.0,0.0,0.0,r],
+                [0.0,1.0,0.0,r],
+                [0.0,0.0,1.0,r],
+                [0.0,0.0,0.0,r]]
 
-# Verification
-nodes = gmsh.model.mesh.getNodes()
-nnodes = len(nodes[0])
-coords = np.reshape(nodes[1], (nnodes,3))
+    create_RVE_particles(domain, particles, mesh_size_factor=0.01, element_order=1)
+    #gmsh.write("periodic_example.vtk")
+    gmsh.write("t_one_incl_15.inp")
 
-face_neg, idx_neg = get_face_nodes(coords, domain[0], 0)
-face_pos, idx_pos = get_face_nodes(coords, domain[1], 0)
+    # Verification
+    nodes = gmsh.model.mesh.getNodes()
+    nnodes = len(nodes[0])
+    coords = np.reshape(nodes[1], (nnodes,3))
 
-plt.figure()
-plt.plot(face_neg[:,1], face_neg[:,2], 'oy', label='xneg')
-plt.plot(face_pos[:,1], face_pos[:,2], '.k', label='xpos')
-plt.legend()
-plt.xlabel('y')
-plt.ylabel('z')
-plt.axis('equal')
+    face_neg, idx_neg = get_face_nodes(coords, domain[0], 0)
+    face_pos, idx_pos = get_face_nodes(coords, domain[1], 0)
 
-face_neg, idx_neg = get_face_nodes(coords, domain[2], 1)
-face_pos, idx_pos = get_face_nodes(coords, domain[3], 1)
+    plt.figure()
+    plt.plot(face_neg[:,1], face_neg[:,2], 'oy', label='xneg')
+    plt.plot(face_pos[:,1], face_pos[:,2], '.k', label='xpos')
+    plt.legend()
+    plt.xlabel('y')
+    plt.ylabel('z')
+    plt.axis('equal')
 
-plt.figure()
-plt.plot(face_neg[:,0], face_neg[:,2], 'oy', label='yneg')
-plt.plot(face_pos[:,0], face_pos[:,2], '.k', label='ypos')
-plt.legend()
-plt.xlabel('x')
-plt.ylabel('z')
-plt.axis('equal')
+    face_neg, idx_neg = get_face_nodes(coords, domain[2], 1)
+    face_pos, idx_pos = get_face_nodes(coords, domain[3], 1)
 
-face_neg, idx_neg = get_face_nodes(coords, domain[4], 2)
-face_pos, idx_pos = get_face_nodes(coords, domain[5], 2)
+    plt.figure()
+    plt.plot(face_neg[:,0], face_neg[:,2], 'oy', label='yneg')
+    plt.plot(face_pos[:,0], face_pos[:,2], '.k', label='ypos')
+    plt.legend()
+    plt.xlabel('x')
+    plt.ylabel('z')
+    plt.axis('equal')
 
-plt.figure()
-plt.plot(face_neg[:,0], face_neg[:,1], 'oy', label='zneg')
-plt.plot(face_pos[:,0], face_pos[:,1], '.k', label='zpos')
-plt.legend()
-plt.xlabel('x')
-plt.ylabel('y')
-plt.axis('equal')
+    face_neg, idx_neg = get_face_nodes(coords, domain[4], 2)
+    face_pos, idx_pos = get_face_nodes(coords, domain[5], 2)
 
-gmsh.finalize()
-plt.show()
+    plt.figure()
+    plt.plot(face_neg[:,0], face_neg[:,1], 'oy', label='zneg')
+    plt.plot(face_pos[:,0], face_pos[:,1], '.k', label='zpos')
+    plt.legend()
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.axis('equal')
+
+    gmsh.finalize()
+    plt.show()

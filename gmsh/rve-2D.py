ee963d62ee4bd6ffd7ffc2ac768fcf94b2c55dd6
gabriel-d-albuquerque <gabriel_albuquerque@usp.br>
2022-04-09 18:41:43 -0300

    Small change to writing path. Porosity value of rve-01 was wrong.

--- gmsh/rve-2D.py
+++ gmsh/rve-2D.py
@@ -2,40 +2,7 @@ import numpy as np
 import gmsh
 import sys
 import os
-
-def create_periodicity(direction, domain, eps=1e-4):
-    if (direction == 0):
-        tx, ty, tz = domain[1] - domain[0], 0.0, 0.0
-        smin = gmsh.model.getEntitiesInBoundingBox(domain[0]-eps, domain[2]-eps, domain[4]-eps, domain[0]+eps, domain[3]+eps, domain[5]+eps, 2)
-
-    if (direction == 1):
-        tx, ty, tz = 0.0, domain[3] - domain[2], 0.0
-        smin = gmsh.model.getEntitiesInBoundingBox(domain[0]-eps, domain[2]-eps, domain[4]-eps, domain[1]+eps, domain[2]+eps, domain[5]+eps, 2)
-
-    if (direction == 2):
-        tx, ty, tz = 0.0, 0.0, domain[5] - domain[4]
-        smin = gmsh.model.getEntitiesInBoundingBox(domain[0]-eps, domain[2]-eps, domain[4]-eps, domain[1]+eps, domain[3]+eps, domain[4]+eps, 2)
-
-    translation = [1, 0, 0, tx, 0, 1, 0, ty, 0, 0, 1, tz, 0, 0, 0, 1]
-
-    for i in smin:
-        xmin, ymin, zmin, xmax, ymax, zmax = gmsh.model.getBoundingBox(i[0], i[1])
-        smax = gmsh.model.getEntitiesInBoundingBox(xmin-eps+tx, ymin-eps+ty, zmin-eps+tz, xmax+eps+tx, ymax+eps+ty, zmax+eps+tz, 2)
-
-        for j in smax:
-            xmin2, ymin2, zmin2, xmax2, ymax2, zmax2 = gmsh.model.getBoundingBox(j[0], j[1])
-            xmin2 -= tx
-            xmax2 -= tx
-            ymin2 -= ty
-            ymax2 -= ty
-            zmin2 -= tz
-            zmax2 -= tz
-            # Apply the periodicity constraint between surfaces meshes
-            if (abs(xmin2 - xmin) < eps and abs(xmax2 - xmax) < eps
-                    and abs(ymin2 - ymin) < eps and abs(ymax2 - ymax) < eps
-                    and abs(zmin2 - zmin) < eps and abs(zmax2 - zmax) < eps):
-                gmsh.model.mesh.setPeriodic(2, [j[1]], [i[1]], translation)
-    return
+from gera_malha_per_cfc import create_periodicity
 
 def create_rve(P, domain, x_c, y_c, mesh_size_factor, path, model):
     if (model == "1"):
@@ -90,15 +57,15 @@ domain = [0, 1, 0, 1, 0, 0]
 x_c = [0, 0, 1, 1, 0.5]
 y_c = [0, 1, 0, 1, 0.5]
 mesh_size_factor = 2e-2
-path = r'C:\Users\gabri\Documents\IC\integration\models'
+path = r'.\models'
 
 create_rve(P, domain, x_c, y_c, mesh_size_factor, path, model = '2')
 
 P = [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7]
 domain = [0, 1, 0, 1, 0, 0]
-x_c = [0]
-y_c = [0]
+x_c = [0.5]
+y_c = [0.5]
 mesh_size_factor = 2e-2
-path = r'C:\Users\gabri\Documents\IC\integration\models'
+path = r'.\models'
 
 create_rve(P, domain, x_c, y_c, mesh_size_factor, path, model = '1')

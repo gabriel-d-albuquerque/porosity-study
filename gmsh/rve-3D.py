d80c705660616a214b137c7292e35563d3c86d68
gabriel-d-albuquerque <gabriel_albuquerque@usp.br>
2022-04-09 18:44:32 -0300

    Porosity range rve-01 was wrong.

--- gmsh/rve-3D.py
+++ gmsh/rve-3D.py
@@ -2,46 +2,13 @@ import numpy as np
 import gmsh
 import sys
 import os
-
-def create_periodicity(direction, domain, eps=1e-4):
-    if (direction == 0):
-        tx, ty, tz = domain[1] - domain[0], 0.0, 0.0
-        smin = gmsh.model.getEntitiesInBoundingBox(domain[0]-eps, domain[2]-eps, domain[4]-eps, domain[0]+eps, domain[3]+eps, domain[5]+eps, 2)
-
-    if (direction == 1):
-        tx, ty, tz = 0.0, domain[3] - domain[2], 0.0
-        smin = gmsh.model.getEntitiesInBoundingBox(domain[0]-eps, domain[2]-eps, domain[4]-eps, domain[1]+eps, domain[2]+eps, domain[5]+eps, 2)
-
-    if (direction == 2):
-        tx, ty, tz = 0.0, 0.0, domain[5] - domain[4]
-        smin = gmsh.model.getEntitiesInBoundingBox(domain[0]-eps, domain[2]-eps, domain[4]-eps, domain[1]+eps, domain[3]+eps, domain[4]+eps, 2)
-
-    translation = [1, 0, 0, tx, 0, 1, 0, ty, 0, 0, 1, tz, 0, 0, 0, 1]
-
-    for i in smin:
-        xmin, ymin, zmin, xmax, ymax, zmax = gmsh.model.getBoundingBox(i[0], i[1])
-        smax = gmsh.model.getEntitiesInBoundingBox(xmin-eps+tx, ymin-eps+ty, zmin-eps+tz, xmax+eps+tx, ymax+eps+ty, zmax+eps+tz, 2)
-
-        for j in smax:
-            xmin2, ymin2, zmin2, xmax2, ymax2, zmax2 = gmsh.model.getBoundingBox(j[0], j[1])
-            xmin2 -= tx
-            xmax2 -= tx
-            ymin2 -= ty
-            ymax2 -= ty
-            zmin2 -= tz
-            zmax2 -= tz
-            # Apply the periodicity constraint between surfaces meshes
-            if (abs(xmin2 - xmin) < eps and abs(xmax2 - xmax) < eps
-                    and abs(ymin2 - ymin) < eps and abs(ymax2 - ymax) < eps
-                    and abs(zmin2 - zmin) < eps and abs(zmax2 - zmax) < eps):
-                gmsh.model.mesh.setPeriodic(2, [j[1]], [i[1]], translation)
-    return
+from gera_malha_per_cfc import create_periodicity
 
 def create_rve(P, domain, x_c, y_c, z_c, mesh_size_factor, path, model):
     if (model == "1"):
-        R = (3*np.array(P)/8*np.pi)**0.5
+        R = ((3*np.array(P))/(4*np.pi))**(1/3)
     elif (model == "2"):
-        R = (3*np.array(P)/(4*np.pi))**0.5
+        R = ((3*np.array(P))/(8*np.pi))**(1/3)
     xmin, xmax = domain[0], domain[1]
     ymin, ymax = domain[2], domain[3]
     zmin, zmax = domain[4], domain[5]
@@ -57,7 +24,7 @@ def create_rve(P, domain, x_c, y_c, z_c, mesh_size_factor, path, model):
         
         if (R[i] !=0):
             for k in range(len(x_c)):
-                sphr = gmsh.model.occ.addShpere(x_c[k], y_c[k], z_c[k], R[i], 10 + k)
+                sphr = gmsh.model.occ.addSphere(x_c[k], y_c[k], z_c[k], R[i], 10 + k)
             rve = gmsh.model.occ.cut([(3, dom)], [(3, t) for t in range(10, 10 + k + 1)])[0][0]
             rve_dim, rve_tag = rve
         else:
@@ -74,32 +41,33 @@ def create_rve(P, domain, x_c, y_c, z_c, mesh_size_factor, path, model):
         
         create_periodicity(0, domain)
         create_periodicity(1, domain)
-        create_periodicity(3, domain)
+        create_periodicity(2, domain)
 
         gmsh.model.mesh.generate()
         
-        file_name = path + r'\rve-0' + model + '-P' + str(i) + '-2D.inp'
+        file_name = path + r'\rve-0' + model + '-P' + str(i) + '-3D.inp'
         os.makedirs(os.path.dirname(file_name), exist_ok=True)
         gmsh.write(file_name)
         
         gmsh.finalize()
     return
 
-P = [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7]
+P = [0, 0.1, 0.2, 0.3, 0.4, 0.5]
 domain = [0, 1, 0, 1, 0, 1]
 x_c = [0, 1, 0, 1, 0, 0, 1, 1, 0.5]
 y_c = [0, 0, 1, 1, 0, 1, 1, 0, 0.5]
 z_c = [0, 0, 0, 1, 1, 1, 0, 1, 0.5]
 mesh_size_factor = 2e-2
-path = r'C:\Users\gabri\Documents\IC\integration\models'
+path = r'.\models'
 
 create_rve(P, domain, x_c, y_c, z_c, mesh_size_factor, path, model = '2')
 
 P = [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7]
-domain = [0, 1, 0, 1, 0, 0]
-x_c = [0]
-y_c = [0]
-mesh_size_factor = 2e-2
-path = r'C:\Users\gabri\Documents\IC\integration\models'
+domain = [0, 1, 0, 1, 0, 1]
+x_c = [0.5]
+y_c = [0.5]
+z_c = [0.5]
+mesh_size_factor = 1e-2
+path = r'.\models'
 
 create_rve(P, domain, x_c, y_c, z_c, mesh_size_factor, path, model = '1')

2fce95d3048dd4d97d89bf6a4cdcf9826879d604
gabriel-d-albuquerque <gabriel_albuquerque@usp.br>
2022-04-29 00:06:24 -0300

    Script cleaned.

--- gmsh/sp_solids.py
+++ gmsh/sp_solids.py
@@ -55,27 +55,6 @@ xyz_trian = [[np.sqrt(2)*L, 0, 0],
 triangle = createPolygon(xyz_trian)
 fuse, map = gmsh.model.occ.fuse([(2, triangle)], fuse)
 
-xyz_trian = [[0, np.sqrt(2)*L/2, np.sqrt(2)*L],
-             [0, np.sqrt(2)*L, np.sqrt(2)*L/2],
-             [0, np.sqrt(2)*L, np.sqrt(2)*L]]
-#triangle = createPolygon(xyz_trian)
-#fuse, map = gmsh.model.occ.fuse([(2, triangle)], fuse)
-
-xyz_trian = [[np.sqrt(2)*L/2, np.sqrt(2)*L, 0],
-             [np.sqrt(2)*L, np.sqrt(2)*L/2, 0],
-             [np.sqrt(2)*L, np.sqrt(2)*L, 0]]
-#triangle = createPolygon(xyz_trian)
-#fuse, map = gmsh.model.occ.fuse([(2, triangle)], fuse)
-
-xyz_trian = [[np.sqrt(2)*L/2, 0, np.sqrt(2)*L],
-             [np.sqrt(2)*L, 0, np.sqrt(2)*L/2],
-             [np.sqrt(2)*L, 0, np.sqrt(2)*L]]
-#triangle = createPolygon(xyz_trian)
-#fuse, map = gmsh.model.occ.fuse([(2, triangle)], fuse)
-
-#tags, coord, param = gmsh.model.occ.getNodes(2, hexagon, includeBoundary = True)
-#norm = 
-
 copy = gmsh.model.occ.copy(fuse)
 gmsh.model.occ.mirror(copy, 1, 0, 0, 0)
 fuse, map = gmsh.model.occ.fuse(fuse, copy)
@@ -102,65 +81,43 @@ ext = gmsh.model.occ.addSurfaceLoop(tags_ext)
 
 gmsh.model.occ.synchronize()
 
-'''
-dims_dilate = []
-tags_dilate = []
-for i in range(len(copy)):
-    dim_i, tag_i = copy[i]
-    dims_dilate.append(dim_i)
-    tags_dilate.append(tag_i)
-dilate = gmsh.model.occ.addSurfaceLoop(tags_dilate)
-'''
-
-#gmsh.model.addPhysicalGroup(2, tags_ext, 1)
-
-#rve = gmsh.model.occ.addVolume([ext, dilate])
 rve = gmsh.model.occ.addVolume([ext])
 copy = gmsh.model.occ.copy([(3, rve)])
-gmsh.model.occ.dilate(copy, 0, 0, 0, 0.7, 0.7, 0.7)
+gmsh.model.occ.dilate(copy, 0, 0, 0, 0.9, 0.9, 0.9)
 offset, map = gmsh.model.occ.cut([(3, rve)], copy)
 
 copy = gmsh.model.occ.copy(offset)
 gmsh.model.occ.translate(copy, x_hex*2, y_hex*2, z_hex*2)
-#fuse, map = gmsh.model.occ.fuse(offset, copy)
 
 copy = gmsh.model.occ.copy(offset)
 gmsh.model.occ.translate(copy, x_hex*2, y_hex*2, -z_hex*2)
-#fuse, map = gmsh.model.occ.fuse(offset, copy)
 
 copy = gmsh.model.occ.copy(offset)
 gmsh.model.occ.translate(copy, x_hex*2, -y_hex*2, z_hex*2)
-#fuse, map = gmsh.model.occ.fuse(offset, copy)
 
 copy = gmsh.model.occ.copy(offset)
 gmsh.model.occ.translate(copy, x_hex*2, -y_hex*2, -z_hex*2)
-#fuse, map = gmsh.model.occ.fuse(offset, copy)
 
 copy = gmsh.model.occ.copy(offset)
 gmsh.model.occ.translate(copy, -x_hex*2, y_hex*2, z_hex*2)
-#fuse, map = gmsh.model.occ.fuse(offset, copy)
 
 copy = gmsh.model.occ.copy(offset)
 gmsh.model.occ.translate(copy, -x_hex*2, y_hex*2, -z_hex*2)
-#fuse, map = gmsh.model.occ.fuse(offset, copy)
 
 copy = gmsh.model.occ.copy(offset)
 gmsh.model.occ.translate(copy, -x_hex*2, -y_hex*2, z_hex*2)
-#fuse, map = gmsh.model.occ.fuse(offset, copy)
 
 copy = gmsh.model.occ.copy(offset)
 gmsh.model.occ.translate(copy, -x_hex*2, -y_hex*2, -z_hex*2)
-#fuse, map = gmsh.model.occ.fuse(offset, copy)
 
 vt = gmsh.model.occ.getEntities(3)
 
 vt, map = gmsh.model.occ.fuse(vt[0:1], vt[1:])
-print(vt)
+
 gmsh.model.occ.synchronize()
 
 gmsh.model.occ.addBox(-np.sqrt(2)*L, -np.sqrt(2)*L, -np.sqrt(2)*L, 2*np.sqrt(2)*L, 2*np.sqrt(2)*L, 2*np.sqrt(2)*L, 10)
 rve, map = gmsh.model.occ.intersect(vt, [(3, 10)])
-print(rve)
 
 gmsh.model.occ.remove_all_duplicates
 
